1b6e5d7d7494ae37eed7e61d2ac53b78b3b56d8c
rzhang294 <rzhang294@euler.wacc.wisc.edu>
2017-12-20 18:27:37 -0600

    debug_build_2

--- MeColDe.cu
+++ MeColDe.cu
@@ -20,6 +20,7 @@ void initializeSize(FILE* fp, int* nVertices, int* nFaces)
 			fscanf(fp, "%1023s", x);
 			if (strcmp(x,"vertex")==0) fscanf(fp, "%d\n", &nVertices[0]);
 			else if (strcmp(x,"face")==0) fscanf(fp, "%d\n", &nFaces[0]);
+			else {fgets(x, 1023, fp);continue;}
 		}
 		if (strcmp(x,"end_header")==0) break;
     	}
@@ -54,12 +55,15 @@ void cross(double* res, double* u, double* v)
 	res[2] = u[0]*v[1] - u[1]*v[0];
 }
 
+/*
 __device__ void d_cross(double* res, double* u, double* v)
 {
         res[0] = u[1]*v[2] - u[2]*v[1];
         res[1] = u[2]*v[0] - u[0]*v[2];
         res[2] = u[0]*v[1] - u[1]*v[0];
 }
+*/
+
 
 
 double dot(double* u, double* v)
@@ -72,7 +76,11 @@ bool intersect(double *p_x, double *p_y, double *p_z, double *tri)
 	double small_num = 0.00000001;
 	double e1[3] = {tri[3]-tri[0], tri[4]-tri[1], tri[5]-tri[2]};
 	double e2[3] = {tri[6]-tri[0], tri[7]-tri[1], tri[8]-tri[2]};
-	double d[3] = {1,0,0};
+
+//	double norm = sqrt(e1[0]*e1[0]+e1[1]*e1[1]+e1[2]*e1[2]);
+//	if (norm == 0.0) {printf("oooh");}
+
+	double d[3] = {0,1,0};
 	double h[3];
 	cross(h, d, e2);
 	double a = dot(e1, h);
@@ -93,15 +101,19 @@ bool intersect(double *p_x, double *p_y, double *p_z, double *tri)
 __global__ void find_volume_normal(int tot_num, double *faces, double *volume, double *normal) {
     	int yourID = blockIdx.x*blockDim.x+threadIdx.x;
 	double *yourTri;
-	yourTri = &(faces[9*yourID]);
+	yourTri = faces + 9*yourID;
 	if (yourID<tot_num) {
 		double d13[3] = {yourTri[3]-yourTri[6], yourTri[4]-yourTri[7], yourTri[5]-yourTri[8]};
 		double d12[3] = {yourTri[0]-yourTri[3], yourTri[1]-yourTri[4], yourTri[2]-yourTri[5]};
 		double cr[3];
-		d_cross(cr, d13, d12);
+		//d_cross(cr, d13, d12);
+		cr[0] = d13[1]*d12[2] - d13[2]*d12[1];
+        	cr[1] = d13[2]*d12[0] - d13[0]*d12[2];
+        	cr[2] = d13[0]*d12[1] - d13[1]*d12[0];
+		
 		double crNorm = sqrt(cr[0]*cr[0]+cr[1]*cr[1]+cr[2]*cr[2]);
 
-		if (crNorm==0) crNorm = 1;	
+//		if (crNorm==0.0) crNorm = 1;	
 
 		double area = 0.5*crNorm;
 		double zMean = (yourTri[2]+yourTri[5]+yourTri[8])/3;
@@ -113,8 +125,9 @@ __global__ void find_volume_normal(int tot_num, double *faces, double *volume, d
 	}
 }
 
-double get_volume(int num_1, int num_2, double* faces_1, double* faces_2, double* center, double* normal_1)
+double get_volume(int num_1, int num_2, double* faces_1, double* faces_2, double* normal_1)
 {
+
 	double *d_faces;  
 	cudaMalloc(&d_faces, sizeof(double)*(num_1+num_2)*9);
 
@@ -126,16 +139,26 @@ double get_volume(int num_1, int num_2, double* faces_1, double* faces_2, double
 	double *h_nor = (double *)malloc(sizeof(double)*(num_1+num_2)*3);
 
 	cudaMemcpy(d_faces,faces_1,sizeof(double)*num_1*9,cudaMemcpyHostToDevice);
-	cudaMemcpy(d_faces+num_1,faces_2,sizeof(double)*num_2*9,cudaMemcpyHostToDevice);
+	cudaMemcpy(d_faces+num_1*9,faces_2,sizeof(double)*num_2*9,cudaMemcpyHostToDevice);
   	find_volume_normal<<<(num_1+num_2+1023)/1024, 1024>>>(num_1+num_2, d_faces, d_vol, d_nor);
   	cudaMemcpy(h_vol, d_vol, sizeof(double)*(num_1+num_2), cudaMemcpyDeviceToHost);
 	cudaMemcpy(h_nor, d_nor, sizeof(double)*(num_1+num_2)*3,  cudaMemcpyDeviceToHost);
 
-	double sum_v = 0.0;
-	for (int i = 0; i<num_1+num_2; ++i) {
+	double sum_v = 0.0, nor_x_tot = 0.0, nor_y_tot = 0.0, nor_z_tot = 0.0;
+	int i;
+	for (i = 0; i<num_1+num_2; ++i) {
 		sum_v = sum_v + h_vol[i];
 	}
-	
+
+	for (i = 0; i<num_1; ++i) {
+		nor_x_tot += h_nor[3*i];
+		nor_y_tot += h_nor[3*i+1];
+		nor_z_tot += h_nor[3*i+2];
+	}
+
+	normal_1[0] = nor_x_tot/num_1;
+	normal_1[1] = nor_y_tot/num_1;
+	normal_1[2] = nor_z_tot/num_1;
 	return sum_v;
 }
 
@@ -161,12 +184,12 @@ int main(int argc, char* argv[]) {
 	initializeArr(fp_2,vertices_x_2,vertices_y_2,vertices_z_2,faces_2,nV_2,nF_2);
 
 // for test reasons
-	double dis = 0.05;
-	for (i=0;i<nV_2;++i) {
-		vertices_x_2[i] += dis;
-		vertices_y_2[i] += dis;
-		vertices_z_2[i] += dis;
-	}
+//	double dis = 0.05;
+//	for (i=0;i<nV_2;++i) {
+//		vertices_x_2[i] += dis;
+//		vertices_y_2[i] += dis;
+//		vertices_z_2[i] += dis;
+//	}
 
 //	printf("%f\n%d\n", *vertices_x_1, *faces_2);
 
@@ -219,10 +242,10 @@ int main(int argc, char* argv[]) {
 	for (i = 0;i<nV_2;++i) {   //index of points that need to know if inside mesh
 		num_ray_intersect = 0;
 		for (j=0;j<nF_1;++j) {  //index of triangles
-			if (((vertices_y_2[i]>face_coord_1[9*j+1])&&(vertices_y_2[i]>face_coord_1[9*j+4])&&(vertices_y_2[i]>face_coord_1[9*j+7])) ||
-			   ((vertices_y_2[i]<face_coord_1[9*j+1])&&(vertices_y_2[i]<face_coord_1[9*j+4])&&(vertices_y_2[i]<face_coord_1[9*j+7])) ||
-			   ((vertices_z_2[i]>face_coord_1[9*j+2])&&(vertices_y_2[i]>face_coord_1[9*j+5])&&(vertices_y_2[i]>face_coord_1[9*j+8])) ||
-                           ((vertices_z_2[i]<face_coord_1[9*j+2])&&(vertices_y_2[i]<face_coord_1[9*j+5])&&(vertices_y_2[i]<face_coord_1[9*j+8]))) {
+			if (((vertices_x_2[i]>face_coord_1[9*j+0])&&(vertices_x_2[i]>face_coord_1[9*j+3])&&(vertices_x_2[i]>face_coord_1[9*j+6])) ||
+			   ((vertices_x_2[i]<face_coord_1[9*j+0])&&(vertices_x_2[i]<face_coord_1[9*j+3])&&(vertices_x_2[i]<face_coord_1[9*j+6])) ||
+			   ((vertices_z_2[i]>face_coord_1[9*j+2])&&(vertices_z_2[i]>face_coord_1[9*j+5])&&(vertices_z_2[i]>face_coord_1[9*j+8])) ||
+                           ((vertices_z_2[i]<face_coord_1[9*j+2])&&(vertices_z_2[i]<face_coord_1[9*j+5])&&(vertices_z_2[i]<face_coord_1[9*j+8]))) {
 				continue;
 			}
 			else if (intersect(vertices_x_2+i, vertices_y_2+i, vertices_z_2+i, face_coord_1+9*j)) {
@@ -235,10 +258,10 @@ int main(int argc, char* argv[]) {
 	for (i = 0;i<nV_1;++i) {   //index of points that need to know if inside mesh
                 num_ray_intersect = 0;
                 for (j=0;j<nF_2;++j) {  //index of triangles
-                        if (((vertices_y_1[i]>face_coord_2[9*j+1])&&(vertices_y_1[i]>face_coord_2[9*j+4])&&(vertices_y_1[i]>face_coord_2[9*j+7])) ||
-                           ((vertices_y_1[i]<face_coord_2[9*j+1])&&(vertices_y_1[i]<face_coord_2[9*j+4])&&(vertices_y_1[i]<face_coord_2[9*j+7])) ||
-                           ((vertices_z_1[i]>face_coord_2[9*j+2])&&(vertices_y_1[i]>face_coord_2[9*j+5])&&(vertices_y_1[i]>face_coord_2[9*j+8])) ||
-                           ((vertices_z_1[i]<face_coord_2[9*j+2])&&(vertices_y_1[i]<face_coord_2[9*j+5])&&(vertices_y_1[i]<face_coord_2[9*j+8]))) {
+                        if (((vertices_x_1[i]>face_coord_2[9*j+0])&&(vertices_x_1[i]>face_coord_2[9*j+3])&&(vertices_x_1[i]>face_coord_2[9*j+6])) ||
+                           ((vertices_x_1[i]<face_coord_2[9*j+0])&&(vertices_x_1[i]<face_coord_2[9*j+3])&&(vertices_x_1[i]<face_coord_2[9*j+6])) ||
+                           ((vertices_z_1[i]>face_coord_2[9*j+2])&&(vertices_z_1[i]>face_coord_2[9*j+5])&&(vertices_z_1[i]>face_coord_2[9*j+8])) ||
+                           ((vertices_z_1[i]<face_coord_2[9*j+2])&&(vertices_z_1[i]<face_coord_2[9*j+5])&&(vertices_z_1[i]<face_coord_2[9*j+8]))) {
                                 continue;
                         }
                         else if (intersect(vertices_x_1+i, vertices_y_1+i, vertices_z_1+i, face_coord_2+9*j)) {
@@ -247,6 +270,24 @@ int main(int argc, char* argv[]) {
                 }
                 if (num_ray_intersect%2==1) { inside_point_set_1[num_point_inside_1] = i; num_point_inside_1++; }
         }
+
+
+	double x_tot = 0.0, y_tot = 0.0, z_tot = 0.0;
+	for (i=0;i<num_point_inside_1;++i) {
+		x_tot += vertices_x_1[inside_point_set_1[i]];
+		y_tot += vertices_y_1[inside_point_set_1[i]];
+		z_tot += vertices_y_1[inside_point_set_1[i]];
+	}
+
+	for (i=0;i<num_point_inside_2;++i) {
+                x_tot += vertices_x_2[inside_point_set_2[i]];
+                y_tot += vertices_y_2[inside_point_set_2[i]];
+                z_tot += vertices_z_2[inside_point_set_2[i]];
+        }
+
+	double center[3] = {x_tot/(num_point_inside_1+num_point_inside_2), 
+				y_tot/(num_point_inside_1+num_point_inside_2), 
+				z_tot/(num_point_inside_1+num_point_inside_2)};
 
 	double *relevant_face_1 = (double *)malloc(sizeof(double)*nF_1*9);
         double *relevant_face_2 = (double *)malloc(sizeof(double)*nF_2*9);
@@ -277,10 +318,9 @@ int main(int argc, char* argv[]) {
 	printf("%f\n", *relevant_face_1); 	
 
 
-	double *center = (double *)malloc(sizeof(double)*3);
 	double *normal_1 = (double *)malloc(sizeof(double)*3);
 
-	double vol = get_volume(num_relevant_face_1, num_relevant_face_2, relevant_face_1, relevant_face_2, center, normal_1);
+	double vol = get_volume(num_relevant_face_1, num_relevant_face_2, relevant_face_1, relevant_face_2, normal_1);
 
   	cudaEventRecord(stopEvent_inc,0);  //ending timing for inclusive
   	cudaEventSynchronize(stopEvent_inc);   
@@ -288,8 +328,9 @@ int main(int argc, char* argv[]) {
  
 
 	printf("%d\n%d\n", nV_1, nF_1);
-
-	printf("%f\n", vol);
+	printf("center is %lf %lf %lf\n", center[0], center[1], center[2]);
+	printf("normal of one direction is %lf %lf %lf\n", normal_1[0], normal_1[1], normal_1[2]);
+	printf("%lf\n", vol);
 	//free resources 
 //	free(in); free(out); free(cuda_out);
 	return 0;
